4d5c08b47e289300ed9341e3de10da65354bfeb9
Martins Lapsa <martinslapsa@gmail.com>
2024-08-08 14:02:28 +0000

    question form

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,30 @@
 class QuestionsController < ApplicationController
+  before_action :set_quiz, only: [:new, :create]
+
   def index
   end
 
   def create
+    @question = @quiz.questions.new(question_params)
+    if @question.save
+      flash.notice = "Question was successfully created."
+      redirect_to quiz_url(@quiz)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def new
+    @question = @quiz.questions.new
+  end
+
+  private
+
+  def set_quiz
+    @quiz = Quiz.find(params[:quiz_id])
+  end
+
+  def question_params
+    params.require(:question).permit(:question_text)
   end
 end
