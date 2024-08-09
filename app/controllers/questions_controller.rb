73f8079b8e27e94a73ece3120d062d79d726c98f
pavels-gaimovics <71257880+pavels-gaimovics@users.noreply.github.com>
2024-08-09 10:41:39 +0000

    Add nested fields

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -6,16 +6,31 @@ class QuestionsController < ApplicationController
 
   def create
     @question = @quiz.questions.new(question_params)
-    if @question.save
-      flash.notice = "Question was successfully created."
-      redirect_to quiz_url(@quiz)
-    else
+
+    if params[:commit] == "add_answer"
+      @question.answers.new
       render :new, status: :unprocessable_entity
+    else
+
+      if @question.save
+        flash.notice = "Question was successfully created."
+        redirect_to quiz_url(@quiz)
+      else
+        render :new, status: :unprocessable_entity
+      end
     end
   end
 
   def new
     @question = @quiz.questions.new
+    @question.answers.new
+  end
+
+  def add_answer
+    @question = @quiz.questions.new(question_params)
+    @question.answers.new
+
+    render :new
   end
 
   private
@@ -25,6 +40,6 @@ class QuestionsController < ApplicationController
   end
 
   def question_params
-    params.require(:question).permit(:question_text)
+    params.require(:question).permit(:question_text, answers_attributes: [:id, :answer_text])
   end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -3,4 +3,5 @@ class Question < ApplicationRecord
 
   belongs_to :quiz
   has_many :answers
+  accepts_nested_attributes_for :answers
 end
